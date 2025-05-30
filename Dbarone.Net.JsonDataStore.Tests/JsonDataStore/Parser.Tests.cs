adfa74d876aacdc21c58fa7ea3f15112c125f300
davidbarone <davidbarone@live.com>
2025-05-30 17:23:30 +1000

    parser

--- Dbarone.Net.JsonDataStore.Tests/JsonDataStore/Parser.Tests.cs
+++ Dbarone.Net.JsonDataStore.Tests/JsonDataStore/Parser.Tests.cs
@@ -15,9 +15,17 @@ public class ParserTests
     [InlineData("+123", "signed_integer", true)]
     [InlineData("1E10", "approximate_numeric_literal", true)]
     [InlineData("1e10", "approximate_numeric_literal", true)]
+    [InlineData("123e-10", "approximate_numeric_literal", true)]
     public void TestGrammar(string input, string rootProductionRule, bool isValid)
     {
         var ast = Parser.Parse(input, rootProductionRule);
+        var a = ast;
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var ast = Parser.Parse("123E-8", "approximate_numeric_literal");
         var a = ast;
     }
 }
